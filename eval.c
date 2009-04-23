aa81f8cdb76c9879a7f3b4c29fed82122cb28163
Joe Damato <ice799@gmail.com>
2009-04-23 12:04:09 -0700

    fixing frame dup bug

--- eval.c
+++ eval.c
@@ -12377,7 +12377,8 @@ rb_thread_start_0(fn, arg, th)
     }
 
     /** set new stack pointer **/
-   __asm__ __volatile__ ("movl %0, %%esp\n" : : "r" (th->stk_base));
+   __asm__ __volatile__ ("movl %0, %%esp\n\t"
+			 "subl $12, %%esp\n" : : "r" (th->stk_base));
    return rb_thread_start_2(fn, arg, th);
 }
 
@@ -12392,6 +12393,7 @@ rb_thread_start_2(fn, arg, th)
    struct BLOCK *volatile saved_block = 0;
    enum rb_thread_status status;
    int state;
+   struct FRAME dummy_frame;
 
   if (ruby_block) {		/* should nail down higher blocks */
 	struct BLOCK dummy;
@@ -12401,6 +12403,10 @@ rb_thread_start_2(fn, arg, th)
 	saved_block = ruby_block = dummy.prev;
     }
     scope_dup(ruby_scope);
+
+    dummy_frame.prev = ruby_frame;
+    frame_dup(&dummy_frame);
+    ruby_frame = dummy_frame.prev;
 
     if (!th->next) {
 	/* merge in thread list */
